faa24b798429620c8bd6ad529bc40700f55d817e
aryan4859 <aryan.shrestha@presidnetial.edu.np>
2025-01-22 13:07:06 -0500

    flag added

--- pcap/main.go
+++ pcap/main.go
@@ -39,10 +39,27 @@ func gardenHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "garden(1).jpg")
 }
 
+func flagHandler(w http.ResponseWriter, r *http.Request) { 
+	file, err := os.Open("flag.png")
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+
+	// Set headers for file download
+	w.Header().Set("Content-Disposition", "attachment; filename=flag.png")
+	w.Header().Set("Content-Type", "image/png")
+
+	// Serve the file
+	http.ServeFile(w, r, "flag.png")
+}
+
 func main() {
 	// Set up the download handler
 	http.HandleFunc("/download", downloadHandler)
 	http.HandleFunc("/garden", gardenHandler)
+	http.HandleFunc("/flag", flagHandler)
 
 	// Start the server on port 8080
 	fmt.Println("Server is running at http://localhost:8080/")
