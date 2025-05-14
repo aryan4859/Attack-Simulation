457e46df325001a6d0184a1ad8924b59e49e6d29
aryan4859 <aryan.shrestha@presidential.edu.np>
2025-05-14 10:47:13 -0400

    fixed path

--- papercuts/public/index.php
+++ papercuts/public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "src/config.php";  // Fixed path
+require_once __DIR__ . "/../src/config.php";  // Use absolute path from current directory
 require_once "includes/header.php";
 include "page.php";
 require_once "includes/footer.php";
