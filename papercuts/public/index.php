44d56028b79e1464296d0e9720f84e5c63d64b97
aryan4859 <aryan.shrestha@presidential.edu.np>
2025-05-14 10:40:06 -0400

    fixed path

--- papercuts/public/index.php
+++ papercuts/public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../src/config.php";
+require_once "src/config.php";  // Fixed path
 require_once "includes/header.php";
 include "page.php";
 require_once "includes/footer.php";
